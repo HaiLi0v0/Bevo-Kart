067a93d7c473d22f5e04e6f0e41f790cbe446623
HaiLi0v0 <hallieho6566@gmail.com>
2026-04-15 13:12:36 -0500

    edited

---
+++ BevoKart_code/Debug/subdir_vars.mk
@@ -0,0 +1,113 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mspm0g3507.cmd 
+
+C_SRCS += \
+C:/BevoKart/inc/ADC1.c \
+C:/BevoKart/inc/Arabic.c \
+C:/BevoKart/inc/Clock.c \
+C:/BevoKart/inc/DAC5.c \
+../LED.c \
+C:/BevoKart/inc/LaunchPad.c \
+C:/BevoKart/inc/SPI.c \
+C:/BevoKart/inc/ST7735.c \
+../SmallFont.c \
+../Sound.c \
+../Switch.c \
+C:/BevoKart/inc/TExaS.c \
+C:/BevoKart/inc/Timer.c \
+../game_main.c \
+../sprites.c 
+
+C_DEPS += \
+./ADC1.d \
+./Arabic.d \
+./Clock.d \
+./DAC5.d \
+./LED.d \
+./LaunchPad.d \
+./SPI.d \
+./ST7735.d \
+./SmallFont.d \
+./Sound.d \
+./Switch.d \
+./TExaS.d \
+./Timer.d \
+./game_main.d \
+./sprites.d 
+
+OBJS += \
+./ADC1.o \
+./Arabic.o \
+./Clock.o \
+./DAC5.o \
+./LED.o \
+./LaunchPad.o \
+./SPI.o \
+./ST7735.o \
+./SmallFont.o \
+./Sound.o \
+./Switch.o \
+./TExaS.o \
+./Timer.o \
+./game_main.o \
+./sprites.o 
+
+OBJS__QUOTED += \
+"ADC1.o" \
+"Arabic.o" \
+"Clock.o" \
+"DAC5.o" \
+"LED.o" \
+"LaunchPad.o" \
+"SPI.o" \
+"ST7735.o" \
+"SmallFont.o" \
+"Sound.o" \
+"Switch.o" \
+"TExaS.o" \
+"Timer.o" \
+"game_main.o" \
+"sprites.o" 
+
+C_DEPS__QUOTED += \
+"ADC1.d" \
+"Arabic.d" \
+"Clock.d" \
+"DAC5.d" \
+"LED.d" \
+"LaunchPad.d" \
+"SPI.d" \
+"ST7735.d" \
+"SmallFont.d" \
+"Sound.d" \
+"Switch.d" \
+"TExaS.d" \
+"Timer.d" \
+"game_main.d" \
+"sprites.d" 
+
+C_SRCS__QUOTED += \
+"C:/BevoKart/inc/ADC1.c" \
+"C:/BevoKart/inc/Arabic.c" \
+"C:/BevoKart/inc/Clock.c" \
+"C:/BevoKart/inc/DAC5.c" \
+"../LED.c" \
+"C:/BevoKart/inc/LaunchPad.c" \
+"C:/BevoKart/inc/SPI.c" \
+"C:/BevoKart/inc/ST7735.c" \
+"../SmallFont.c" \
+"../Sound.c" \
+"../Switch.c" \
+"C:/BevoKart/inc/TExaS.c" \
+"C:/BevoKart/inc/Timer.c" \
+"../game_main.c" \
+"../sprites.c" 
+
+
---
+++ BevoKart_code/game_main.c
@@ -0,0 +1,135 @@
+// Lab9Main.c
+// Runs on MSPM0G3507
+// Lab 9 ECE319K
+// Your name
+// Last Modified: January 12, 2026
+
+#include <stdio.h>
+#include <stdint.h>
+#include <ti/devices/msp/msp.h>
+#include "../inc/ST7735.h"
+#include "../inc/Clock.h"
+#include "../inc/LaunchPad.h"
+#include "../inc/TExaS.h"
+#include "../inc/Timer.h"
+#include "../inc/ADC1.h"
+#include "../inc/DAC5.h"
+#include "../inc/Arabic.h"
+#include "SmallFont.h"
+#include "LED.h"
+#include "Switch.h"
+#include "Sound.h"
+#include "images/images.h"
+//#include "images.h"
+
+typedef enum{
+  START,
+  PLAY,
+  P1_WIN,
+  P2_WIN
+} GameState_t;
+
+GameState_t State;
+
+struct sprite {
+  int32_t x;      // x coordinate
+  int32_t y;      // y coordinate
+  int32_t xold, yold;
+  int32_t vx,vy;  // pixels/30Hz
+  const unsigned short *image; // ptr->image
+  //const unsigned short *black;
+  status_t life;        // dead/alive
+  int32_t w; // width
+  int32_t h; // height
+  uint32_t needDraw; // true if need to draw
+};
+typedef struct sprite sprite_t;
+
+// ****note to ECE319K students****
+// the data sheet says the ADC does not work when clock is 80 MHz
+// however, the ADC seems to work on my boards at 80 MHz
+// I suggest you try 80MHz, but if it doesn't work, switch to 40MHz
+void PLL_Init(void){ // set phase lock loop (PLL)
+  // Clock_Init40MHz(); // run this line for 40MHz
+  Clock_Init80MHz(0);   // run this line for 80MHz
+}
+
+uint32_t M=1;
+uint32_t Random32(void){
+  M = 1664525*M+1013904223;
+  return M;
+}
+uint32_t Random(uint32_t n){
+  return (Random32()>>16)%n;
+}
+
+
+// games  engine runs at 30Hz
+void TIMG12_IRQHandler(void){uint32_t pos,msg;
+  if((TIMG12->CPU_INT.IIDX) == 1){ // this will acknowledge
+    GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
+    GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
+// game engine goes here
+    // 1) sample slide pot
+    // 2) read input switches
+    // 3) move sprites
+    // 4) start sounds
+    // 5) set semaphore
+    // NO LCD OUTPUT IN INTERRUPT SERVICE ROUTINES
+    GPIOB->DOUTTGL31_0 = GREEN; // toggle PB27 (minimally intrusive debugging)
+  }
+}
+uint8_t TExaS_LaunchPadLogicPB27PB26(void){
+  return (0x80|((GPIOB->DOUT31_0>>26)&0x03));
+}
+
+// use main2 to observe graphics
+int main2(void){ // main2
+  __disable_irq();
+  PLL_Init(); // set bus speed
+  LaunchPad_Init();
+  ST7735_InitPrintf(INITR_REDTAB); // INITR_REDTAB for AdaFruit, INITR_BLACKTAB for HiLetGo
+    //note: if you colors are weird, see different options for
+    // ST7735_InitR(INITR_REDTAB); inside ST7735_InitPrintf()
+  ST7735_FillScreen(ST7735_BLACK);
+  ST7735_DrawBitmap(22, 159, PlayerShip0, 18,8); // player ship bottom
+  ST7735_DrawBitmap(53, 151, Bunker0, 18,5);
+  ST7735_DrawBitmap(42, 159, PlayerShip1, 18,8); // player ship bottom
+  ST7735_DrawBitmap(62, 159, PlayerShip2, 18,8); // player ship bottom
+  ST7735_DrawBitmap(82, 159, PlayerShip3, 18,8); // player ship bottom
+  ST7735_DrawBitmap(0, 9, SmallEnemy10pointA, 16,10);
+  ST7735_DrawBitmap(20,9, SmallEnemy10pointB, 16,10);
+  ST7735_DrawBitmap(40, 9, SmallEnemy20pointA, 16,10);
+  ST7735_DrawBitmap(60, 9, SmallEnemy20pointB, 16,10);
+  ST7735_DrawBitmap(80, 9, SmallEnemy30pointA, 16,10);
+
+  for(uint32_t t=500;t>0;t=t-5){
+    SmallFont_OutVertical(t,104,6); // top left
+    Clock_Delay1ms(50);              // delay 50 msec
+  }
+  ST7735_FillScreen(0x0000);   // set screen to black
+  ST7735_SetCursor(1, 1);
+  ST7735_OutString("GAME OVER");
+  ST7735_SetCursor(1, 2);
+  ST7735_OutString("Nice try,");
+  ST7735_SetCursor(1, 3);
+  ST7735_OutString("Earthling!");
+  ST7735_SetCursor(2, 4);
+  ST7735_OutUDec(1234);
+  while(1){
+  }
+}
+
+// use main3 to test switches and LEDs
+int main3(void){ // main3
+  __disable_irq();
+  PLL_Init(); // set bus speed
+  LaunchPad_Init();
+  Switch_Init(); // initialize switches
+  LED_Init(); // initialize LED
+  while(1){
+    // write code to test switches and LEDs
+    
+  }
+}
+
